7fafc2fbb7fc5c609e37a1da49b66e0ec9dcbf09
Louis-Lcq <83230080+Louis-Lcq@users.noreply.github.com>
2023-02-06 14:32:18 +0100

    update

--- Projet_GestionAppel/src/main/java/ctrl/LoginController.java
+++ Projet_GestionAppel/src/main/java/ctrl/LoginController.java
@@ -13,56 +13,56 @@ import java.util.HashMap;
 @WebServlet(name="/LoginController" , value="/login")
 public class LoginController extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession(false);
 		if(session!=null){
 			session.invalidate();
 		}
 		response.sendRedirect("Login");
-    }
+	}
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        HashMap<String, String> erreurs = new HashMap<>();
+		HashMap<String, String> erreurs = new HashMap<>();
 
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
 
-        if (email == null || email.isEmpty()) {
-            erreurs.put("email_error", "Veillez entrer l'addresse email");
-        }
-        if (password == null || password.isEmpty()) {
-            erreurs.put("password_error", "Veillez entrer un mot de passe");
-        }
+		if (email == null || email.isEmpty()) {
+			erreurs.put("email_error", "Veillez entrer l'addresse email");
+		}
+		if (password == null || password.isEmpty()) {
+			erreurs.put("password_error", "Veillez entrer un mot de passe");
+		}
 
-        if (erreurs.isEmpty()) {
-            try {
-            	UsersDao usersDao = new UsersDao();
+		if (erreurs.isEmpty()) {
+			try {
+				UsersDao usersDao = new UsersDao();
 				Users usersId = usersDao.loginUtilisateur(email, password);
-                if (usersId == null) {
-                    request.setAttribute("generale_error", "Email ou mot de passe incorrect ! Veuillez réessayer !");
-                    request.getRequestDispatcher("Login").forward(request, response);
-                } else {
-                    request.getSession().setAttribute("auth", usersId);
+				if (usersId == null) {
+					request.setAttribute("generale_error", "Email ou mot de passe incorrect ! Veuillez réessayer !");
+					request.getRequestDispatcher("Login").forward(request, response);
+				} else {
+					request.getSession().setAttribute("auth", usersId);
 					request.getSession().setAttribute("id", usersId.getId());
 					request.getSession().setAttribute("email", usersId.getEmail());
 					request.getSession().setAttribute("nom", usersId.getNom());
 					request.getSession().setAttribute("prenom", usersId.getPrenom());
 					request.getSession().setAttribute("statut", usersId.getStatut());
 					response.sendRedirect("Accueil");
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                request.setAttribute("generale_error", "Probleme technique ! Veuillez contacter l'administrateur.");
-                request.getRequestDispatcher("Login").forward(request, response);
-            }
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				request.setAttribute("generale_error", "Probleme technique ! Veuillez contacter l'administrateur.");
+				request.getRequestDispatcher("Login").forward(request, response);
+			}
 
-        } else {
-            erreurs.forEach(request::setAttribute);
-            request.getRequestDispatcher("Login").forward(request, response);
-        }
-    }
+		} else {
+			erreurs.forEach(request::setAttribute);
+			request.getRequestDispatcher("Login").forward(request, response);
+		}
+	}
 
 }
